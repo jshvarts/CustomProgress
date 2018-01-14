8dddc4d6a6334c44e39abdc0a53db2a8e8cda5be
James Shvarts <jshvarts@gmail.com>
2018-01-14 14:44:30 -0500

    add determinate horizontal progress bar

--- app/src/main/java/com/jshvarts/customprogress/MainActivity.kt
+++ app/src/main/java/com/jshvarts/customprogress/MainActivity.kt
@@ -2,10 +2,25 @@ package com.jshvarts.customprogress
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    private val itemCount = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadingProgressBar.max = itemCount
+
+        observeProgress()
+                .concatMap { Observable.just(it).delay(50, TimeUnit.MILLISECONDS) }
+                .subscribe ( { loadingProgressBar.progress++ }, Timber::e)
+    }
+
+    private fun observeProgress(): Observable<Int> {
+        return Observable.range(1, itemCount)
     }
 }
